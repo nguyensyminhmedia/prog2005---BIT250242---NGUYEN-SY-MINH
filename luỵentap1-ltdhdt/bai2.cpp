128ad5d58cca0cbf38d51dadc4b30bf2a719f34d
nguyensyminhmedia <nguyensyminhmedia@gmail.com>
2026-05-20 14:12:45 +0700

    Add files via upload

---
+++ luỵentap1-ltdhdt/bai2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float w, h, dienTich;
+
+    cout << "Nhap chieu rong: ";
+    cin >> w;
+
+    cout << "Nhap chieu cao: ";
+    cin >> h;
+
+    dienTich = w * h;
+
+    cout << "Dien tich = " << dienTich;
+
+    return 0;
+}
