836340c545cee986fff5170c848df725eee41296
nguyensyminhmedia <nguyensyminhmedia@gmail.com>
2026-05-24 17:26:45 +0700

    Add files via upload

---
+++ Cơ sở lập trình/bai8-lt2.py
@@ -0,0 +1,8 @@
+a = int(input("Nhập số thứ nhất: "))
+b = int(input("Nhập số thứ hai: "))
+
+if b == 0:
+    print("Không thể chia cho 0")
+else:
+    result = a / b
+    print("Kết quả là:", result)
