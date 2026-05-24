0c97ed209b2ed246a05e3e6b7773556a290af0e8
nguyensyminhmedia <nguyensyminhmedia@gmail.com>
2026-05-24 17:12:04 +0700

    Add files via upload

---
+++ Cơ sở lập trình/bai2-lt2.py
@@ -0,0 +1,10 @@
+# Nhập 3 số từ bàn phím
+a = int(input("Nhập số thứ nhất: "))
+b = int(input("Nhập số thứ hai: "))
+c = int(input("Nhập số thứ ba: "))
+
+# Tìm số lớn nhất
+max_value = max(a, b, c)
+
+# In kết quả
+print("Số lớn nhất là:", max_value)
